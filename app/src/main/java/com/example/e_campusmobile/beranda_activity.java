1224ba878068a067eee35e3c97fa04b097b262b1
RizkyFirdaus <rizkyfirdaoes8.0@gmail.com>
2022-11-02 09:42:48 +0700

    Membuat halaman login

--- app/src/main/java/com/example/e_campusmobile/beranda_activity.java
+++ app/src/main/java/com/example/e_campusmobile/beranda_activity.java
@@ -3,15 +3,12 @@ package com.example.e_campusmobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class beranda_activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_beranda);
     }
-
-
 }
